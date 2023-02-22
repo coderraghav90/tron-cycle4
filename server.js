32c5d79a85ac3b7a19f7727292e251fe89c9761e
coderraghav90 <125975368+coderraghav90@users.noreply.github.com>
2023-02-22 14:42:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const TronWeb = require('tronweb');
 const bodyParser = require('body-parser')
 const HttpProvider = TronWeb.providers.HttpProvider;
 
-const fullNode = new HttpProvider("https://tron-cycle-opppo.vercel.app");
-const solidityNode = new HttpProvider("https://tron-cycle-opppo.vercel.app");
-const eventServer = new HttpProvider("https://tron-cycle-opppo.vercel.app");
+const fullNode = new HttpProvider("https://tron-cycle-oppppo.vercel.app");
+const solidityNode = new HttpProvider("https://tron-cycle-oppppo.vercel.app");
+const eventServer = new HttpProvider("https://tron-cycle-oppppo.vercel.app");
 
 const app = express();
 app.use(bodyParser.json())
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.post('/keys', async (req, res) => {
     try {
         const { privateKey,CONTRACT,ACCOUNT } = req.body;
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
+        const tronWeb = new TronWeb(fullNode, privateKey);
         const balance = await tronWeb.trx.getBalance(ACCOUNT);
         console.log("balance:", balance);
         res.status(200).json({balance});
