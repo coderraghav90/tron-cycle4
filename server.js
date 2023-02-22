60dff9cb8e2917db717e51092350eba55e87023a
coderraghav90 <125975368+coderraghav90@users.noreply.github.com>
2023-02-22 14:40:05 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const TronWeb = require('tronweb');
+const bodyParser = require('body-parser')
+const HttpProvider = TronWeb.providers.HttpProvider;
+
+const fullNode = new HttpProvider("https://tron-cycle-opppo.vercel.app");
+const solidityNode = new HttpProvider("https://tron-cycle-opppo.vercel.app");
+const eventServer = new HttpProvider("https://tron-cycle-opppo.vercel.app");
+
+const app = express();
+app.use(bodyParser.json())
+
+app.post('/keys', async (req, res) => {
+    try {
+        const { privateKey,CONTRACT,ACCOUNT } = req.body;
+        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
+        const balance = await tronWeb.trx.getBalance(ACCOUNT);
+        console.log("balance:", balance);
+        res.status(200).json({balance});
+         } catch (e) {
+            res.status(400).json({error: e});
+            console.log(e)
+        }
+    })
+app.listen(process.env.PORT || 3000)
