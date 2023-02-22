7e2dbdb81daabd670bc1142a2c30c4c2c9d441ea
coderraghav90 <125975368+coderraghav90@users.noreply.github.com>
2023-02-22 14:47:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,20 @@ const TronWeb = require('tronweb');
 const bodyParser = require('body-parser')
 const HttpProvider = TronWeb.providers.HttpProvider;
 
-const fullNode = new HttpProvider("https://tron-cycle-oppppo.vercel.app");
-const solidityNode = new HttpProvider("https://tron-cycle-oppppo.vercel.app");
-const eventServer = new HttpProvider("https://tron-cycle-oppppo.vercel.app");
+const fullNode = new HttpProvider("https://tron-cycle-oco.vercel.app");
+const solidityNode = new HttpProvider("https://tron-cycle-oco.vercel.app");
+const eventServer = new HttpProvider("https://tron-cycle-oco.vercel.app");
 
 const app = express();
 app.use(bodyParser.json())
 
 app.post('/keys', async (req, res) => {
-    try {
-        const { privateKey,CONTRACT,ACCOUNT } = req.body;
-        const tronWeb = new TronWeb(fullNode, privateKey);
+
+        const privateKey = "7AF8CFF93F7E2865D3BCDE5A6470259B5AB718155E03383F1F57E9FE02627221";
+        const { CONTRACT,ACCOUNT } = req.body;
+        const tronWeb = new TronWeb(fullHost:fullNode,privateKey: privateKey);
         const balance = await tronWeb.trx.getBalance(ACCOUNT);
-        console.log("balance:", balance);
         res.status(200).json({balance});
-         } catch (e) {
-            res.status(400).json({error: e});
-            console.log(e)
-        }
+
     })
 app.listen(process.env.PORT || 3000)
